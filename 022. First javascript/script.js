f2c6b380f81595cbb8080590c7a3b208e909ea95
StefanW24 <90267126+StefanW24@users.noreply.github.com>
2021-11-25 12:41:42 +0100

    Add files via upload

---
+++ 022. First javascript/script.js
@@ -0,0 +1,34 @@
+// -------------- punt 4 ----------------//
+
+console.log("Hello Winc Academy");
+
+// -------------- punt 5 ----------------//
+
+let name = 'Stefan';
+console.log(name);
+
+// -------------- punt 6 ----------------//
+
+// variable called if is not possible----//
+
+// -------------- punt 7 ----------------//
+
+console.log(4 + 4);
+console.log("4" + "4");
+
+// -------------- punt 8 ----------------//
+
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 5);
+
+// -------------- punt 9 ----------------//
+
+let age = 48;
+console.log(typeof 48);
+
+// -------------- punt 10 ---------------//
+
+// ?? 
